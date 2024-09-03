83e3b49e0d4d6f26f66178ec2e44072fb621d1f7
kyleenicolas <167857373+kyleenicolas@users.noreply.github.com>
2024-09-04 01:10:33 +0800

    Add files via upload

---
+++ data table.sql
@@ -0,0 +1,28 @@
+create table data table (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	gender VARCHAR(50),
+	ip_address VARCHAR(20)
+);
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (1, 'Rockwell', 'Paddon', 'rpaddon0@taobao.com', 'Male', '113.116.183.255');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (2, 'Louise', 'Schulter', 'lschulter1@bloomberg.com', 'Female', '8.183.117.239');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (3, 'Brien', 'Clarae', 'bclarae2@printfriendly.com', 'Male', '125.255.100.58');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (4, 'Appolonia', 'Whife', 'awhife3@upenn.edu', 'Female', '117.71.234.78');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (5, 'Henrietta', 'Polye', 'hpolye4@skyrock.com', 'Female', '168.62.195.153');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (6, 'Amble', 'Riggott', 'ariggott5@forbes.com', 'Male', '114.18.208.154');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (7, 'Hugues', 'Dallewater', 'hdallewater6@a8.net', 'Male', '139.103.166.201');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (8, 'Alex', 'Woolis', 'awoolis7@fema.gov', 'Male', '2.209.81.4');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (9, 'Emma', 'Kirkham', 'ekirkham8@parallels.com', 'Female', '210.105.67.253');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (10, 'Horst', 'Janas', 'hjanas9@squidoo.com', 'Male', '31.148.184.76');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (11, 'Tabor', 'Audsley', 'taudsleya@umn.edu', 'Male', '190.245.167.132');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (12, 'Mireille', 'Paur', 'mpaurb@bbc.co.uk', 'Female', '145.250.210.203');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (13, 'Cesare', 'Jeffes', 'cjeffesc@google.fr', 'Male', '32.240.22.250');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (14, 'Ad', 'Courcey', 'acourceyd@zdnet.com', 'Male', '29.92.160.59');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (15, 'Devon', 'Birdseye', 'dbirdseyee@squidoo.com', 'Female', '153.59.213.118');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (16, 'Urban', 'Benza', 'ubenzaf@miitbeian.gov.cn', 'Male', '54.81.144.5');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (17, 'Tamera', 'Whisby', 'twhisbyg@yale.edu', 'Female', '220.171.107.237');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (18, 'Dede', 'Edmands', 'dedmandsh@hubpages.com', 'Female', '147.144.41.51');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (19, 'Idalina', 'Lahrs', 'ilahrsi@harvard.edu', 'Female', '22.5.176.182');
+insert into data table (id, first_name, last_name, email, gender, ip_address) values (20, 'Parry', 'Checcucci', 'pcheccuccij@businessinsider.com', 'Male', '195.106.241.43');
